0059c303778901220d8732619a1285f9da4de2c3
Admin <Admin@192.168.1.37>
2026-02-23 19:34:06 +0530

    Commit-Changes

---
+++ selenium-basics/src/main/java/utilities/ExcelUtility.java
@@ -0,0 +1,48 @@
+package utilities;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelUtility {
+	static FileInputStream f; 
+	static XSSFWorkbook w; 
+	static XSSFSheet s;
+	
+	public static String getStringData(int a,int b,String sheet) throws IOException 
+	{
+	// to get string (Name) from the excel 
+	// int a int b -> 	to get the data from the row (a) and columns's (b)index 
+		f=new FileInputStream("C:\\Users\\Admin\\git\\repository\\selenium-basics\\src\\test\\resources\\TestData.xlsx");
+	//new for memory allocation -> this is for inputting the file 
+		w=new XSSFWorkbook(f);
+		//to get the workbook and store it in w. 
+		s=w.getSheet(sheet);// Sheet1 -> Sheet name 
+		//getsheet -> Inbuilt method to get the excel sheet. 
+		//to get the sheet from the workbook.
+		/* XSSFRow inbuilt class to identify the Row*/
+		XSSFRow r=s.getRow(a); //Inbuilt method to get the Row. 
+		/* XSSFCell inbuilt class to identify the Cell*/
+		XSSFCell c=r.getCell(b); // Inbuilt method to get the Cell. (Row and Column should be given as 0,1,2,3)
+		//r.cell is because, we need to take the column from the corresponding row. 
+		return c.getStringCellValue(); //get the String value as string... inbuilt method 
+	}
+	public static int  getIntegerData(int a,int b, String sheet) throws IOException 
+	{
+	// to get int (Salary) from the excel 
+	// int a int b -> 	to get the data from the row (a) and columns's (b)index 
+		f=new FileInputStream("C:\\Users\\Admin\\git\\repository\\selenium-basics\\src\\test\\resources\\TestData.xlsx");
+		w=new XSSFWorkbook(f);
+		s=w.getSheet("Sheet1");
+		XSSFRow r=s.getRow(a);
+		XSSFCell c=r.getCell(b);
+		//type casting -> convert one data type to another. 
+		int y = (int) c.getNumericCellValue();
+		// c/getNumericCellValue - value inside c is returned as double. then we convert to int (type casting)
+		return y;
+	}
+}
--- selenium-basics/src/test/java/seleniumBasics/HandlingWebElements.java
+++ selenium-basics/src/test/java/seleniumBasics/HandlingWebElements.java
@@ -1,7 +1,15 @@
 package seleniumBasics;
 
+import java.time.Duration;
+import java.util.NoSuchElementException;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HandlingWebElements extends Base
 
@@ -15,6 +23,14 @@ public class HandlingWebElements extends Base
 		msgbox.sendKeys("Welcome"); //to input value
 		
 		WebElement showmsg = driver.findElement(By.id("button-one"));
+		//Explicit Wait
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.elementToBeClickable(showmsg));
+		//Fluent Wait
+		Wait <WebDriver> fluentWait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(5))
+		.pollingEvery(Duration.ofSeconds(1)).ignoring(NoSuchElementException.class);
+		fluentWait.until(ExpectedConditions.elementToBeClickable(showmsg));
+
 		showmsg.click();
 		
 		WebElement displaymsg = driver.findElement(By.id("message-one"));
